1920e52ffd3c5e0a544a5789fc8f6c0e3da15c2b
Tatu91 <60737752+Tatu91@users.noreply.github.com>
2020-10-23 14:52:15 +0300

    Update merkkilaskintests.cs

--- Merkkijonolaskintests/merkkilaskintests.cs
+++ Merkkijonolaskintests/merkkilaskintests.cs
@@ -1,18 +1,66 @@
+using MerkkijonoLaskin;
 using NUnit.Framework;
+using System;
 
-namespace Merkkijonolaskintests
+namespace MerkkijonoLaskinTests
 {
-    public class Tests
+    public class MerkkijonoLaskinTests
     {
+        MerkkijonoLaskin laskin;
         [SetUp]
         public void Setup()
         {
+            laskin = new MerkkijonoLaskin();
         }
 
         [Test]
-        public void Test1()
+        public void TyhjaMerkkijonoPalauttaaNollan()
         {
-            Assert.Pass();
+            Assert.AreEqual(0, laskin.Laske(""));
+
         }
+
+        [Test]
+        public void LaskinPalauttaaAnnetunArvonJosVainYksiNumero()
+        {
+
+            Assert.AreEqual(5, laskin.Laske("5"));
+        }
+
+        [Test]
+        public void LaskinPalauttaaKahdenPilkullaErotetunLuvunSumman()
+        {
+            Assert.AreEqual(9, laskin.Laske("5,4"));
+        }
+
+        [Test]
+        public void LaskinPalauttaaUseammanPilkullaErotetunLuvunSumman()
+        {
+            Assert.AreEqual(30, laskin.Laske("5,4,10,11"));
+        }
+
+        [Test]
+        public void LaskinPalauttaaLukujenSummanNewlineMerkillaErotettuna()
+        {
+            Assert.AreEqual(5, laskin.Laske("2\n3"));
+            Assert.AreEqual(6, laskin.Laske("1,2\n3"));
+            Assert.AreEqual(6, laskin.Laske("1,2\\n3"));
+        }
+
+        [Test]
+        public void NegatiivinenLukuPienimpiKuinNollaAiheuttaaVirheen()
+        {
+            Assert.Throws<ArgumentException>(() => {
+                laskin.Laske("-1");
+            });
+        }
+
+        [Test]
+        public void LaskinEiHuomioTuhannenYlittaviaLukuja()
+        {
+            Assert.AreEqual(0, laskin.Laske("1001"));
+            Assert.AreEqual(4, laskin.Laske("1,1001\n3"));
+        }
+
     }
 }
