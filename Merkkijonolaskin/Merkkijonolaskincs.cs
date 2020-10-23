8831ee6eac90e0c5437128a568c0969cc73638e9
Tatu91 <60737752+Tatu91@users.noreply.github.com>
2020-10-23 14:33:24 +0300

    Merkkijonolaskin

---
+++ Merkkijonolaskin/Merkkijonolaskincs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merkkijonolaskin
+{
+    public class MerkkijonoLaskin
+    {
+        public MerkkijonoLaskin()
+        {
+        }
+        public int Laske(string syote)
+        {
+            if (OnkoTyhjaSyote(syote))
+                return 0;
+
+            string[] erotinmerkit = new string[] { ",", "\n", "\\n" };
+            string[] numerot = syote.Split(erotinmerkit, StringSplitOptions.RemoveEmptyEntries);
+
+            int summa = 0;
+            foreach (string yksittainenNumero in numerot)
+            {
+                int luku = int.Parse(yksittainenNumero);
+
+                if (LukuOnPienempiKuinNolla(luku))
+                    throw new ArgumentException("Luku ei voi olla negatiivinen");
+
+
+                if (LukuOnSuurempiKuinTuhat(luku))
+                    continue;
+
+                summa += luku;
+            }
+            return summa;
+        }
+
+        private bool OnkoTyhjaSyote(string syote)
+        {
+            return syote.Length == 0;
+        }
+
+        private bool LukuOnSuurempiKuinTuhat(int luku)
+        {
+            return luku > 1000;
+        }
+
+        private bool LukuOnPienempiKuinNolla(int luku)
+        {
+            return luku < 0;
+        }
+    }
+
+
+}
---
+++ Merkkijonolaskin/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace Merkkijonolaskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merkkijonolaskin v1.0");
+            Console.WriteLine("Anna luvut pilkulla erotettuna: ");
+
+            string syote = Console.ReadLine();
+
+            Merkkijonolaskin laskin = new Merkkijonolaskin();
+            Console.WriteLine("Lukujen summa on: " + laskin.Laske(syote));
+        }
+    }
+}
+
